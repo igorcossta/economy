e04f76023362e71332938f682b19bb61423b00d2
igorcossta <65612587+igorcossta@users.noreply.github.com>
2025-05-10 12:54:48 -0300

    Feat: improve deposit command logic and feedback

--- src/main/java/com/github/igorcossta/command/DepositCommand.java
+++ src/main/java/com/github/igorcossta/command/DepositCommand.java
@@ -2,11 +2,19 @@ package com.github.igorcossta.command;
 
 import com.github.igorcossta.command.template.Command;
 import com.github.igorcossta.domain.service.Deposit;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Objects;
+
+import static io.papermc.paper.command.brigadier.Commands.argument;
 
 class DepositCommand extends Command {
     private final Deposit deposit;
@@ -24,13 +32,41 @@ class DepositCommand extends Command {
     public LiteralArgumentBuilder<CommandSourceStack> run(LiteralArgumentBuilder<CommandSourceStack> argumentBuilder) {
         return argumentBuilder
                 .requires(commandSourceStack -> commandSourceStack.getExecutor() instanceof Player)
-                .executes(commandContext -> {
-                    Player player = (Player) commandContext.getSource().getSender();
-                    BigDecimal amount = new BigDecimal("100.5");
+                .then(argument("username", StringArgumentType.string())
+                        .suggests((context, builder) -> {
+                            Bukkit.getOnlinePlayers()
+                                    .stream()
+                                    .map(Player::getName)
+                                    .forEach(builder::suggest);
+                            return builder.buildFuture();
+                        })
+                        .then(argument("amount", DoubleArgumentType.doubleArg())
+                                .executes(context -> {
+                                    String username = StringArgumentType.getString(context, "username");
+                                    double amount = DoubleArgumentType.getDouble(context, "amount");
 
-                    deposit.to(player.getUniqueId(), amount);
-                    player.sendMessage("Added to your account $%s".formatted(amount));
+                                    Player commandExecutor = (Player) context.getSource().getSender();
+
+                                    try {
+                                        Player target = Bukkit.getPlayerExact(username);
+                                        Objects.requireNonNull(target, "player must be online");
+
+                                        deposit.to(target.getUniqueId(), new BigDecimal(amount));
+                                        commandExecutor.sendMessage("sending to %s amount %s".formatted(username, NumberFormat.getCurrencyInstance(Locale.US).format(amount)));
+                                    } catch (Exception e) {
+                                        commandExecutor.sendMessage(e.getMessage());
+                                    }
+                                    return 1;
+                                }) // when all arguments are passed
+                        ).executes(context -> {
+                            Player commandExecutor = (Player) context.getSource().getSender();
+                            commandExecutor.sendMessage("you need to enter the amount");
+                            return 1;
+                        }) // if amount argument is not specified
+                ).executes(context -> {
+                    Player commandExecutor = (Player) context.getSource().getSender();
+                    commandExecutor.sendMessage("you need to enter player name");
                     return 1;
-                });
+                }); // if no argument is provided
     }
 }
