7b5bb835092b93432ea79cc2d6f26f3274b4ed81
igorcossta <65612587+igorcossta@users.noreply.github.com>
2025-05-11 14:48:53 -0300

    Build: add junit and mockito dependencies

---
+++ src/test/java/com/github/igorcossta/EconomyTest.java
@@ -0,0 +1,16 @@
+package com.github.igorcossta;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class EconomyTest {
+    @Test
+    void shouldPass() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void shouldFail() {
+        Assertions.assertFalse(false);
+    }
+}
