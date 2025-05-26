836efa479d83d0cbde4f72cdbffee40f8185c4e8
igorcossta <65612587+igorcossta@users.noreply.github.com>
2025-05-26 10:58:49 -0300

    Feat: allow players to view others' balances

--- src/main/java/com/github/igorcossta/command/BalanceCommand.java
+++ src/main/java/com/github/igorcossta/command/BalanceCommand.java
@@ -3,8 +3,12 @@ package com.github.igorcossta.command;
 import com.github.igorcossta.command.template.Command;
 import com.github.igorcossta.domain.service.Balance;
 import com.github.igorcossta.utils.Format;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 class BalanceCommand extends Command {
@@ -23,8 +27,29 @@ class BalanceCommand extends Command {
     public LiteralArgumentBuilder<CommandSourceStack> run(LiteralArgumentBuilder<CommandSourceStack> argumentBuilder) {
         return argumentBuilder
                 .requires(commandSourceStack -> commandSourceStack.getExecutor() instanceof Player)
-                .executes(commandContext -> {
-                    Player player = (Player) commandContext.getSource().getSender();
+                .then(Commands.argument("username", StringArgumentType.string())
+                        .suggests((context, builder) -> {
+                            Bukkit.getOnlinePlayers()
+                                    .stream()
+                                    .map(Player::getName)
+                                    .forEach(builder::suggest);
+                            return builder.buildFuture();
+                        })
+                        .executes(context -> {
+                            String username = StringArgumentType.getString(context, "username");
+                            OfflinePlayer target = Bukkit.getOfflinePlayer(username);
+
+                            Player commandExecutor = (Player) context.getSource().getSender();
+                            try {
+                                commandExecutor.sendMessage("%s balance is %s".formatted(username,
+                                        Format.money(balance.show(target.getUniqueId()))));
+                            } catch (Exception e) {
+                                commandExecutor.sendMessage(e.getMessage());
+                            }
+                            return 1;
+                        })
+                ).executes(context -> {
+                    Player player = (Player) context.getSource().getSender();
                     player.sendMessage("Your balance is %s".formatted(Format.money(balance.show(player.getUniqueId()))));
                     return 1;
                 });
